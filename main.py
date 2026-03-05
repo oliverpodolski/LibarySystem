29c4cd5806a8d308761e9c0471f59ddd5c84fc77
OliverPodolski <Oliverpodolski2008@gmail.com>
2026-03-05 22:21:38 +0100

    Added search function by using the name of a book or author

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
+from dataclasses import dataclass
 from json import JSONDecodeError
+from turtledemo.clock import datum
 
 file_path = "data.json"
 
@@ -84,8 +86,31 @@ def delete_book(remove_book_id):
         json.dump(data, file, indent=4)
     main_menu()
 
+def search_for_book(input_text):
+    input_text = input_text.lower()
+    found_books = 0
+    if not os.path.exists(file_path):
+        print("No file was found")
+        create_file()
+        return
+    with open(file_path, "r") as file:
+        try:
+            data = json.load(file)
+        except JSONDecodeError:
+            print("There are no books in the file saved!")#
+            return main_menu()
+        for i in range(len(data)):
+            book_name = data[i]["Book"]
+            book_author = data[i]["Author"]
+            if input_text in book_name.lower() or input_text in book_author.lower():
+                print(f"{i}. Book: {data[i]['Book']} Author: {data[i]['Author']} Release Year: {data[i]['Release Year']} ID: {data[i]['ID']}")
+                found_books += 1
+        if found_books == 0:
+            print(f"There was no book or author found with the name: {input_text}")
+        main_menu()
+
 def main_menu():
-    main_menu_option = int(input("\nWhat would you like to do?\n1. ADD A BOOK\n2. READ BOOK LIST\n3. REMOVE A BOOK"))
+    main_menu_option = int(input("\nWhat would you like to do?\n1. ADD A BOOK\n2. READ BOOK LIST\n3. REMOVE A BOOK\n4. SEARCH FOR A BOOK\n"))
     if main_menu_option == 1:
         get_name = input("Enter the name of the Book: ")
         get_release_year = int(input("Enter the release year: "))
@@ -96,6 +121,9 @@ def main_menu():
     elif main_menu_option == 3:
         get_remove_book_id = int(input("Enter the Book_ID:"))
         delete_book(get_remove_book_id)
+    elif main_menu_option == 4:
+        get_search_text = input("Enter the name of the book or the author you are looking for:\n")
+        search_for_book(get_search_text)
     else:
         print("Enter a valid choice!")
 
